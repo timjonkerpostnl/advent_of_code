45ebfaec64119425fd1c4b1c2e1a530b67ed6636
Tim Jonker <85954232+timjonkerpostnl@users.noreply.github.com>
2023-12-04 07:47:14 +0100

    fix end and more memory efficient

--- src/day4/assignment2.py
+++ src/day4/assignment2.py
@@ -1,30 +1,25 @@
 import re
+from collections import defaultdict
 from typing import List
 
 from src.day4.assignment1 import find_numbers
 
 
-def process_cards(num_cards, wins_per_card):
-    for card, wins in wins_per_card.items():
-        for i in range(wins):
-            num_cards[card + 1 + i] += num_cards[card]
-    return num_cards
-
-
 def process_file(file_name: str) -> int:
-    wins_per_card = {}
-    num_cards = {}
+    num_cards = defaultdict(int)
+    lines = 0
     with open(file_name) as f:
         for idx, line in enumerate(f):
+            lines += 1
             stripped_line = line.strip()
             winning_numbers, my_numbers = find_numbers(stripped_line)
             present_winning_numbers = winning_numbers.intersection(my_numbers)
-            wins_per_card[idx+1] = len(present_winning_numbers)
-            num_cards[idx + 1] = 1
+            wins = len(present_winning_numbers)
+            num_cards[idx + 1] += 1
+            for i in range(wins):
+                num_cards[idx + 2 + i] += num_cards[idx + 1]
 
-    num_cards = process_cards(num_cards, wins_per_card)
-
-    summed = sum(num_cards.values())
+    summed = sum(x for i, x in enumerate(num_cards.values()) if i <= lines)
 
     return summed
 
--- src/day4/test/test_assignment2.py
+++ src/day4/test/test_assignment2.py
@@ -4,5 +4,6 @@ import pytest
 
 from src.day4.assignment2 import process_file
 
+
 def test_assignment2():
     assert process_file("input_test1.txt") == 30
